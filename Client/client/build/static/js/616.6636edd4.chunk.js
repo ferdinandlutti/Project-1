aecabf28d8c29ffb91cecc5610db92c84dc85100
Ferdinand Luttichau <ferdinandvluttichau@gmail.com>
2024-02-26 13:01:17 +0100

    latest

---
+++ Client/client/build/static/js/616.6636edd4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkclient=self.webpackChunkclient||[]).push([[616],{616:(e,c,s)=>{s.r(c),s.d(c,{default:()=>t});const t={}}}]);
+//# sourceMappingURL=616.6636edd4.chunk.js.map
